92a48aa4bf2072f343974b562e84c4d79ee51ec9
lordgittgenstein <gitty.litter@gmail.com>
2016-06-29 20:51:02 +0530

    Add files via upload

--- static/file-ext.js
+++ static/file-ext.js
@@ -1,1 +1,28 @@
+var _validFileExtensions = [".pdf", ".jpg", ".jpeg", ".png"];
 
+function Validate(oForm) {
+    var arrInputs = oForm.getElementsByTagName("input");
+    for (var i = 0; i < arrInputs.length; i++) {
+        var oInput = arrInputs[i];
+        if (oInput.type == "file") {
+            var sFileName = oInput.value;
+            if (sFileName.length > 0) {
+                var blnValid = false;
+                for (var j = 0; j < _validFileExtensions.length; j++) {
+                    var sCurExtension = _validFileExtensions[j];
+                    if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+                        blnValid = true;
+                        break;
+                    }
+                }
+
+                if (!blnValid) {
+                    alert("Document is invalid, allowed file extensions are: " + _validFileExtensions.join(", "));
+                    return false;
+                }
+            }
+        }
+    }
+
+    return confirm('Once saved you cannot change details online. Have you checked the text and details?');
+}
